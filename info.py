5d813942508a93a8e9fc3bde0a342b8f7c7a2f80
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-02-22 09:07:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,9 +101,9 @@ URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'MrAK_LinkZ_bot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'MrAK_LinkZ_bot'))
+name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
